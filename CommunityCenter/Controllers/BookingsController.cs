ac53e100275265dfe9fc03e9e504cad3ece7d420
Debbrath <36687868+sujonnath@users.noreply.github.com>
2025-08-31 14:15:43 +0600

    Create ASP MVC Project with API and AngularJS UI

---
+++ CommunityCenter/Controllers/BookingsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CommunityCenter.Models; // Change this to your actual namespace
+
+
+namespace CommunityCenter.Controllers
+{
+    public class BookingsController : ApiController
+    {
+        private CommunityCenterEntities db = new CommunityCenterEntities();
+
+        // GET: api/Bookings
+        //public IQueryable<Booking> GetBookings()
+        //{
+        //    return db.Bookings;
+        //}
+
+        public IHttpActionResult GetBookings()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+
+            var bookings = db.Bookings.ToList();
+            return Ok(bookings);
+        }
+
+        // GET: api/Bookings/5
+        [ResponseType(typeof(Booking))]
+        public IHttpActionResult GetBooking(int id)
+        {
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(booking);
+        }
+
+        // POST: api/Bookings
+        [ResponseType(typeof(Booking))]
+        public IHttpActionResult PostBooking(Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Bookings.Add(booking);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = booking.Id }, booking);
+        }
+
+        // PUT: api/Bookings/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBooking(int id, Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != booking.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(booking).State = System.Data.Entity.EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch
+            {
+                if (!BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Bookings/5
+        [ResponseType(typeof(Booking))]
+        public IHttpActionResult DeleteBooking(int id)
+        {
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            db.Bookings.Remove(booking);
+            db.SaveChanges();
+
+            return Ok(booking);
+        }
+
+        private bool BookingExists(int id)
+        {
+            return db.Bookings.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ CommunityCenter/Scripts/app.js
@@ -0,0 +1,45 @@
+﻿var app = angular.module("bookingApp", []);
+
+app.controller("BookingController", function ($scope, $http) {
+    const apiUrl = "/api/bookings";
+
+    $scope.bookings = [];
+    $scope.newBooking = {};
+
+    // Load all bookings
+    $scope.loadBookings = function () {
+        $http.get(apiUrl)
+            .then(function (response) {
+                $scope.bookings = response.data;
+            }, function (error) {
+                alert("Error loading bookings.");
+            });
+    };
+
+    // Add booking
+    $scope.addBooking = function () {
+        $http.post(apiUrl, $scope.newBooking)
+            .then(function (response) {
+                alert("Booking added.");
+                $scope.newBooking = {};
+                $scope.loadBookings();
+            }, function (error) {
+                alert("Error adding booking.");
+            });
+    };
+
+    // Delete booking
+    $scope.deleteBooking = function (id) {
+        if (confirm("Are you sure you want to delete this booking?")) {
+            $http.delete(apiUrl + "/" + id)
+                .then(function (response) {
+                    alert("Booking deleted.");
+                    $scope.loadBookings();
+                }, function (error) {
+                    alert("Error deleting booking.");
+                });
+        }
+    };
+
+    $scope.loadBookings(); // Initial load
+});
